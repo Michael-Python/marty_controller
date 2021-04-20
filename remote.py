2416d09bb434ccbd7e96050b028919d87c86d572
Michael Sinclair <70299684+Michael-Python@users.noreply.github.com>
2021-04-20 16:13:38 +0100

    Add files via upload

---
+++ remote.py
@@ -0,0 +1,40 @@
+from martypy import Marty
+import tkinter as tk
+marty = Marty('exp', '/dev/ttyAMA0')
+marty.get_ready()
+
+def key_input(event):
+    key_press = event.keysym.lower()
+    if key_press == 'w':
+        print('Marty walks two steps forward')
+        marty.walk()
+    elif key_press == 'a':
+        print('Marty walks two steps left')
+        marty.sidestep('left')
+    elif key_press == 's':
+        print('Marty walks two steps backward')
+        marty.walk(2, 'auto', 0, -25)
+    elif key_press == 'd':
+        print('Marty walks two steps right')
+        marty.sidestep('right')
+    elif key_press == 'q':
+        print('Marty turns 15 degrees left') 
+        marty.walk(2, 'auto', 15)
+    elif key_press == 'e':
+        print('Marty turns 16 2/3 degrees right')
+        marty.walk(2, 'auto', -15)
+    elif key_press == 'z':
+        print('Marty stands straight')
+        marty.stand_straight()
+    elif key_press == 'c':
+        print('Marty dances')
+        marty.dance()
+    elif key_press == 'x':
+        print('Marty plays excited sound')
+        marty.play_sound('excited')
+    marty.stop('pause')
+
+
+keys  = tk.Tk()
+keys.bind_all('<Key>', key_input)
+keys.mainloop()s
